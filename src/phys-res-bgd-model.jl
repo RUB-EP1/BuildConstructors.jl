12069fde182a278b87b7abc0ad31842e596ff3f4
Robert Hentges <robert.hentges@cern.ch>
2025-11-27 10:30:05 +0100

    fixed support logic in convert_db, added ser/deser for AdvPar, corrected truncation in PRBmodel

--- src/phys-res-bgd-model.jl
+++ src/phys-res-bgd-model.jl
@@ -9,7 +9,7 @@
         p = build_model(_.model_p, pars)
         r = build_model(_.model_r, pars)
         b = build_model(_.model_b, pars)
-        r_conv_p = fft_convolve(r, p)
-        truncated(MixtureModel([r_conv_p, b], [fs, 1-fs]), _.support[1], _.support[2])
+        r_conv_p = truncated(fft_convolve(r, p), _.support[1], _.support[2])
+        MixtureModel([r_conv_p, b], [fs, 1-fs])
     end
 )
